a511c18b45b39b93295cdfc02cd493e0fa20469e
jianxian <36605259+mengxianbo@users.noreply.github.com>
2025-07-04 15:56:06 +0800

    Update cron-glitch.py

--- cron-glitch.py
+++ cron-glitch.py
@@ -21,7 +21,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # 定义默认URL
-DEFAULT_URL = "https://officetv.glitch.me/"
+DEFAULT_URL = "https://olive-hospitable-red.glitch.me/"
 URL = DEFAULT_URL
 
 # 创建cookies目录
